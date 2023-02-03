ed4d76ecad6a2b79f0ec456c9999d83820964736
Miguel Figuera <miguelqui725@gmail.com>
2023-02-03 17:38:51 -0400

    final commit with all set and done

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,10 +2,22 @@ class KittensController < ApplicationController
 
 def index
     @kittens=Kitten.all
+
+    respond_to do |format|
+        format.html
+        format.json {render :json =>@kittens}
+        format.xml {render :xml=>@kittens}
+    end
 end
 
 def show
     @kitten=Kitten.find(params[:id])
+
+    respond_to do |format|
+        format.html
+        format.json {render :json =>@kitten}
+        format.xml {render :xml=>@kitten}
+    end
 end
 
 def new
@@ -16,10 +28,9 @@ def create
     @kitten=Kitten.new(kitten_params)
 
     if @kitten.save!
-        redirect_to @kitten
-        flash[:notice] = "Post successfully created"
+        redirect_to @kitten, notice: 'Kitty Succefully Created'
     else
-        render :new, status: :unprocessable_entity
+        render :new, status: :unprocessable_entity, alert: 'Your Cat NO PASS!'
     end
 end
 
@@ -32,9 +43,9 @@ def update
     @kitten=Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
-        redirect_to @kitten
+        redirect_to @kitten, notice: 'Your kitty has been updated.'
     else
-        render :edit, status: :unprocessable_entity
+        render :edit, status: :unprocessable_entity, alert: 'Your Kitty has NOT been saved... SAVE THE KITTY PLEASE!'
     end
 end
 
@@ -43,7 +54,6 @@ def delete
     @kitten.delete
 
     redirect_to root_path, status: :see_other
-
 end
 
 private
